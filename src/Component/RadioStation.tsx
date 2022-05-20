302303444de2325d817e3545cff00fa8db3e51cd
kamruz-zzaman <kamruz.zaman8991@gmail.com>
2022-05-20 18:42:34 +0600

    handle station click added

--- src/Component/RadioStation.tsx
+++ src/Component/RadioStation.tsx
@@ -1,29 +1,41 @@
+import { useState } from 'react';
 import BackArrow from '../assets/back-arrow.png'
 import SwitchIcon from '../assets/switch.png'
 import Styles from './stationStyle.module.css'
+const station: { id: number, station_name: string, freq: string }[] = [
+    {
+        id: 1,
+        station_name: "Putin FM",
+        freq: "66,6"
+    },
+    {
+        id: 2,
+        station_name: "Dribbble FM",
+        freq: "101,2"
+    },
+    {
+        id: 3,
+        station_name: "Doge FM",
+        freq: "99,4"
+    },
+    {
+        id: 4,
+        station_name: "Ballads FM",
+        freq: "87,1"
+    },
+    {
+        id: 5,
+        station_name: "Maximum FM",
+        freq: "142,2"
+    }
+]
 const RadioStation = () => {
-    const station: { station_name: string, freq: string }[] = [
-        {
-            station_name: "Putin FM",
-            freq: "66,6"
-        },
-        {
-            station_name: "Dribbble FM",
-            freq: "101,2"
-        },
-        {
-            station_name: "Doge FM",
-            freq: "99,4"
-        },
-        {
-            station_name: "Ballads FM",
-            freq: "87,1"
-        },
-        {
-            station_name: "Maximum FM",
-            freq: "142,2"
-        }
-    ]
+    const [clickData, setClickData] = useState({})
+    const handleStation = (id: number) => {
+        setClickData(station[id - 1])
+    }
+    console.log(clickData);
+
     return (
         <section>
             <div className={Styles.container}>
@@ -41,7 +53,7 @@ const RadioStation = () => {
                 <div className={Styles.station_Container}>
                     {
                         station.map((data) => (
-                            <div className={Styles.stations}>
+                            <div key={data.id} onClick={() => handleStation(data.id)} className={Styles.stations}>
                                 <p>{data.station_name}</p>
                                 <p>{data.freq}</p>
                             </div>
