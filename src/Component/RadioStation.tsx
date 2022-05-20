a4fe73955961f17fdb0d966894a0b2e3c353afe3
kamruz-zzaman <kamruz.zaman8991@gmail.com>
2022-05-20 18:29:03 +0600

    add station from array

--- src/Component/RadioStation.tsx
+++ src/Component/RadioStation.tsx
@@ -2,42 +2,52 @@ import BackArrow from '../assets/back-arrow.png'
 import SwitchIcon from '../assets/switch.png'
 import Styles from './stationStyle.module.css'
 const RadioStation = () => {
+    const station: { station_name: string, freq: string }[] = [
+        {
+            station_name: "Putin FM",
+            freq: "66,6"
+        },
+        {
+            station_name: "Dribbble FM",
+            freq: "101,2"
+        },
+        {
+            station_name: "Doge FM",
+            freq: "99,4"
+        },
+        {
+            station_name: "Ballads FM",
+            freq: "87,1"
+        },
+        {
+            station_name: "Maximum FM",
+            freq: "142,2"
+        }
+    ]
     return (
         <section>
             <div className={Styles.container}>
                 <div className={Styles.header}>
                     <div className={Styles.icon}>
-                        <img width='50%' src={BackArrow} alt="" />
+                        <img width='40%' src={BackArrow} alt="" />
                     </div>
                     <div className={Styles.title}>
                         <p>STATIONS</p>
                     </div>
                     <div className={Styles.icon}>
-                        <img width='50%' src={SwitchIcon} alt="" />
+                        <img width='40%' src={SwitchIcon} alt="" />
                     </div>
                 </div>
                 <div className={Styles.station_Container}>
-                    <div className={Styles.stations}>
-                        <p>Putin FM</p>
-                        <p>66,6</p>
-                    </div>
-                    <div className={Styles.stations}>
-                        <p>Dribbble FM</p>
-                        <p>101,2</p>
-                    </div>
-                    <div className={Styles.stations}>
-                        <p>Doge FM</p>
-                        <p>99,4</p>
-                    </div>
-                    <div className={Styles.stations}>
-                        <p>Ballads FM</p>
-                        <p>87,1</p>
-                    </div>
-                    <div className={Styles.stations}>
-                        <p>Maximum FM</p>
-                        <p>142,2</p>
-                    </div>
-                    <div style={{ height: "50px" }}>
+                    {
+                        station.map((data) => (
+                            <div className={Styles.stations}>
+                                <p>{data.station_name}</p>
+                                <p>{data.freq}</p>
+                            </div>
+                        ))
+                    }
+                    <div className={Styles.bottom_station}>
 
                     </div>
                 </div>
